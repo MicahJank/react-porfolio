5a71e693cd75ba2e6d6c167a1cf7a089197e106e
MicahJank <MicahJank@users.noreply.github.com>
2020-02-17 15:45:19 -0600

    added button for getting to portfolio

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,15 +1,43 @@
 import React from 'react';
 
 import styled from 'styled-components';
-import { Image } from 'semantic-ui-react';
+import { Image, Header, Button } from 'semantic-ui-react';
 
 import micahLogo from '../../imgs/Micah-Svg.svg';
 
 const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     .ui.medium.centered.image {
         margin-top: 4rem;
     }
+
+    .ui.center.header {
+        font-size: 6rem;
+        color: #eaeaea;
+    }
+
+    .ui.center.header.subHeader {
+        font-size: 3rem;
+    }
+
+    .ui.massive.primary.button {
+        margin-top: 4rem;
+        width: 25%;
+        font-size: 3rem;
+        padding: 3rem;
+        background-color: #43B3E0;
+        text-shadow: -1px -1px 0.5px rgba(0, 0, 0, 0.5);
+        box-shadow: inset 0px 0px 0px 1px rgba(0, 0, 0, 0.5), inset 0px 2px 1px 0px rgba(255, 255, 255, 0.75);
+        border-radius: 8px;
+        
+        :hover {
+            background-color: #46c4ea;
+        }
+    }
+
 `;
 
 const Intro = () => {
@@ -17,6 +45,9 @@ const Intro = () => {
     return (
         <Container>
             <Image centered size='medium' src={micahLogo} />
+            <Header textAlign='center'>Hi. I'm Micah Jank.</Header>
+            <Header className='subHeader' textAlign='center'>a Web Developer.</Header>
+            <Button className='portfolio' primary size='massive'>Check Out My Work</Button>
         </Container>
     )
 };
