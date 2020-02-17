c6644d097025d0474dc030fed8c0c869aab0242f
MicahJank <MicahJank@users.noreply.github.com>
2020-02-16 21:36:37 -0600

    added nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Container } from 'semantic-ui-react';
 
+import NavigationBar from './components/Navigation/NavigationBar.js';
+
 function App() {
   return (
     <Container fluid>
-      
+      <NavigationBar />
     </Container>
   );
 }
--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Input, Menu } from 'semantic-ui-react';
 
 const NavigationBar = () => {
+    const [active, setActive] = useState('Top');
+    
+    const handleClick = (e, { name }) => {
+        setActive(name);
+    }
 
     return (
-        <div>Navigation bar</div>
+        <Menu size='huge' secondary widths='12'>
+            <Menu.Item 
+                name='Top'
+                active={active === 'Top'}
+                onClick={handleClick}
+            />
+             <Menu.Item 
+                name='Portfolio'
+                active={active === 'Portfolio'}
+                onClick={handleClick}
+            />
+             <Menu.Item 
+                name='Contact'
+                active={active === 'Contact'}
+                onClick={handleClick}
+            />
+        </Menu>
     )
 };
 
