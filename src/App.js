f7096bf58bc20bc47f481d6f2f2f27c3dd9fda6c
MicahJank <MicahJank@users.noreply.github.com>
2020-02-17 12:19:49 -0600

    refactored the nav styles into the navigation bar component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 
 
 const Container = styled.div`
-  display: flex;
   background: linear-gradient(to bottom, rgb(146, 20, 30), rgb(212, 21, 37) );
   height: 100vh;
 `;
--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -2,6 +2,21 @@ import React, { useState } from 'react';
 
 import { Input, Menu } from 'semantic-ui-react';
 
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+    .ui.menu {
+        background-color: #2b2b2b;
+    }
+    .ui.menu a.item, .ui.menu a.item.active {
+        color: white;
+
+        :hover {
+        color: white;
+        }
+    }
+`;
+
 const NavigationBar = () => {
     const [active, setActive] = useState('');
     
@@ -10,23 +25,25 @@ const NavigationBar = () => {
     }
 
     return (
-        <Menu size='huge' secondary widths='12' stackable>
-            <Menu.Item 
-                name='Top'
-                active={active === 'Top'}
-                onClick={handleClick}
-            />
-             <Menu.Item 
-                name='Portfolio'
-                active={active === 'Portfolio'}
-                onClick={handleClick}
-            />
-             <Menu.Item 
-                name='Contact'
-                active={active === 'Contact'}
-                onClick={handleClick}
-            />
-        </Menu>
+        <Nav>
+            <Menu size='huge' secondary widths='12' stackable>
+                <Menu.Item
+                    name='Top'
+                    active={active === 'Top'}
+                    onClick={handleClick}
+                />
+                <Menu.Item 
+                    name='Portfolio'
+                    active={active === 'Portfolio'}
+                    onClick={handleClick}
+                />
+                <Menu.Item 
+                    name='Contact'
+                    active={active === 'Contact'}
+                    onClick={handleClick}
+                />
+            </Menu>
+        </Nav>
     )
 };
 
