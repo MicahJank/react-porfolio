ed3a9b88656814d2dbfc313b35176dd82d8b3aa1
MicahJank <MicahJank@users.noreply.github.com>
2020-02-18 16:22:05 -0600

    added portfolio component and project card component

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ProjectCard from './ProjectCard.js';
+
+const Portfolio = () => {
+
+    return (
+        <div>Portfolio</div>
+    )
+};
+
+export default Portfolio;
