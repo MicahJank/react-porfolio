a6d84ca73a966b142128695b8770b8df8090c667
MicahJank <MicahJank@users.noreply.github.com>
2020-09-22 21:29:44 -0500

    working on adding tech stack to the projects

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -163,8 +163,8 @@ const Portfolio = (props) => {
                 <Header className="title-sm"><span>Click</span> a project to see more info!</Header>
                 <div className='cards'>
                     <ProjectCard deployment="https://tidyhive.vercel.app" github="https://github.com/MicahJank/homerun-fe/blob/master/README.md" img={tidyHive} title={'TidyHive'} description={'TidyHive is a home organizational tool where members of a household can create, assign, and complete tasks.'} />
-                    <ProjectCard img={expatJournal} title={'Expat Journal'} description={''} />
-                    <ProjectCard img={githubUserCard} title={'Github User Search'} description={''} />
+                    <ProjectCard deployment="https://expat-journal-savareen.netlify.app/#/sign-in" github="https://github.com/MicahJank/expat-journal/blob/master/README.md" img={expatJournal} title={'Expat Journal'} description={'Expat Journal is a place for travelers and expats to share their experiences.'} />
+                    <ProjectCard deployment="https://github-user-search-tawny.vercel.app/" github="https://github.com/MicahJank/github-user-search/blob/master/README.md" img={githubUserCard} title={'Github User Search'} description={'An easy and quick way to find users and their followers.'} />
                 </div>
                 <div className="bottom-section">
                     <Header className="title">like what you see?</Header>
--- src/components/Portfolio/ProjectModal.js
+++ src/components/Portfolio/ProjectModal.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 import { Modal, Button, Header, Image, Embed, Divider, Icon} from 'semantic-ui-react';
+import node from '../../imgs/nodejs.svg';
+import oauth from '../../imgs/oauth.svg';
+import postgresql from '../../imgs/postgresql.svg';
+import react from '../../imgs/react.svg';
+import reactrouter from '../../imgs/reactrouter.svg';
+import redux from '../../imgs/redux.svg';
+import semanticui from '../../imgs/semanticui.svg';
 import styled from 'styled-components';
 
 const LowerSection = styled.div`
@@ -15,17 +22,54 @@ const LowerSection = styled.div`
     }
     
 `;
+
+const Placeholder = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 400px;
+
+    background-image: url('https://react.semantic-ui.com/images/wireframe/image.png');
+    background-position: center;
+    background-size: cover;
+
+    .demo-video-text {
+        font-size: 4rem !important;
+        text-align: center;
+    }      
+`;
+
+const TechStack = styled.div`
+
+    .tech {
+        width: 70px;
+    }
+  
+`;
+
 const ProjectModal = props => {
 
     return (
         <>
             <Modal.Header>
-                <Embed id='O6Xo21L0ybE' source='youtube' />
+                {/* <Embed id='O6Xo21L0ybE' source='youtube' /> */}
+                <Placeholder>
+                    <Header className='demo-video-text' as='h1' content="Demo Video Coming Soon"/>
+                </Placeholder>
             </Modal.Header>
+            <TechStack>
+                <Divider horizontal>
+                    {/* <FontAwesomeIcon className="tech" icon={faReact} /> */}
+                    <img src={react} className='tech' alt='react svg logo' />
+                    <img src={redux} className='tech redux' alt='redux svg logo' />
+
+                </Divider>
+            </TechStack>
             <Modal.Content className={'middle-section'}>
                 <Modal.Description>
                     <Header as="h1">{props.title}</Header>
-                    <h3>{props.description}</h3>
+                    <h2>{props.description}</h2>
                 </Modal.Description>
             </Modal.Content>
             <Divider/>
