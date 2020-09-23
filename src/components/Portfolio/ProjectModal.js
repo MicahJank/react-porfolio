d46e8e44fe170ab8468d18ac75cce39b3d5e8aad
MicahJank <MicahJank@users.noreply.github.com>
2020-09-23 12:15:13 -0500

    made it so the project modals have tech stack listed

--- src/components/Portfolio/ProjectModal.js
+++ src/components/Portfolio/ProjectModal.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Modal, Button, Header, Image, Embed, Divider, Icon} from 'semantic-ui-react';
 import node from '../../imgs/nodejs.svg';
 import oauth from '../../imgs/oauth.svg';
-import postgresql from '../../imgs/postgresql.svg';
+import postgre from '../../imgs/postgresql.svg';
 import react from '../../imgs/react.svg';
 import reactrouter from '../../imgs/reactrouter.svg';
 import redux from '../../imgs/redux.svg';
-import semanticui from '../../imgs/semanticui.svg';
+import semantic from '../../imgs/semanticui.svg';
 import styled from 'styled-components';
 
 const LowerSection = styled.div`
@@ -41,15 +41,72 @@ const Placeholder = styled.div`
 `;
 
 const TechStack = styled.div`
-
-    .tech {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .tech.react {
         width: 70px;
+        @media screen and (max-width: 500px) {
+          width: 35px;
+        }
+    }
+    .tech.redux {
+        height: 40px;
+        margin-right: 10px;
+        @media screen and (max-width: 500px) {
+            height: 20px;
+          }
+    }
+    .tech.semantic {
+        width: 40px;
+        @media screen and (max-width: 500px) {
+            width: 20px;
+          }
+    }
+    .tech.reactrouter {
+        width: 70px;
+        margin-left: 20px;
+        @media screen and (max-width: 500px) {
+            width: 35px;
+          }
+    }
+    .tech.oauth {
+        width: 50px;
+        @media screen and (max-width: 500px) {
+            width: 25px;
+          }
+    }
+    .tech.node {
+        width: 45px;
+        margin-left: 10px;
+        @media screen and (max-width: 500px) {
+            width: 20px;
+          }
+    }
+    .tech.postgre {
+        width: 40px;
+        margin-right: 10px;
+        @media screen and (max-width: 500px) {
+            width: 20px;
+          }
     }
   
 `;
 
 const ProjectModal = props => {
+    const [icons, setIcons] = useState({});
 
+    useEffect(() => {
+        setIcons({
+            node,
+            react,
+            redux,
+            semantic,
+            oauth,
+            postgre,
+            reactrouter
+        })
+    },[])
     return (
         <>
             <Modal.Header>
@@ -58,14 +115,22 @@ const ProjectModal = props => {
                     <Header className='demo-video-text' as='h1' content="Demo Video Coming Soon"/>
                 </Placeholder>
             </Modal.Header>
-            <TechStack>
-                <Divider horizontal>
+            <Divider horizontal>
+                <TechStack>
                     {/* <FontAwesomeIcon className="tech" icon={faReact} /> */}
-                    <img src={react} className='tech' alt='react svg logo' />
+                    {props.tech ? props.tech.map(item => {
+                        
+                        return <img key={item} src={icons[item]} className={`tech ${item}`} alt={`${item} svg logo`} />
+                    }) : ''}
+                    {/* <img src={oauth} className='tech oauth' alt='oauth svg logo' />
+                    <img src={semanticui} className='tech semanticui' alt='semantic ui svg logo' />
+                    <img src={react} className='tech react' alt='react svg logo' />
                     <img src={redux} className='tech redux' alt='redux svg logo' />
+                    <img src={reactrouter} className='tech reactrouter' alt='react router svg logo' />
+                    <img src={node} className='tech nodejs' alt='node svg logo' /> */}
 
-                </Divider>
-            </TechStack>
+                </TechStack>
+            </Divider>
             <Modal.Content className={'middle-section'}>
                 <Modal.Description>
                     <Header as="h1">{props.title}</Header>
@@ -75,11 +140,11 @@ const ProjectModal = props => {
             <Divider/>
             <Modal.Content>
                 <LowerSection>
-                <Button as="a" href={props.github} size='massive' icon labelPosition='left'>
+                <Button color="black" as="a" href={props.github} size='massive' icon labelPosition='left'>
                 <Icon name='github' />
                 Repository
                 </Button>
-                <Button as="a" href={props.deployment} size='massive' icon labelPosition='left'>
+                <Button color="olive" as="a" href={props.deployment} size='massive' icon labelPosition='left'>
                 <Icon name='desktop' />
                 Deployment
                 </Button>
